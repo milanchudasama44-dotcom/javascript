0e3fe61714f05db4c74f9973f730c3535b296d64
unknown <you@example.com>
2026-01-28 12:25:46 +0530

    [CM] [datatype & variable]

---
+++ datatypes.js
@@ -0,0 +1,149 @@
+// two types of data types
+//1. promitive 
+// number 
+// BigInt
+// String
+// Boolean
+// Symbol
+// null
+// undefined
+
+
+//2. non-primitive
+
+// Array
+// object
+// function
+
+// let num=1;
+// let num2=2;
+
+// console.log("num",num,num2);
+
+// let name="milan";
+// let name2="vishal";
+
+// console.log("names :",name2,name)
+// let isTrue =true;
+// let isFalse=false;
+// console.log("boolean :",isTrue)
+
+//undefined
+
+// let a;
+// console.log("undefined :",a)
+
+
+// let BigInt=12345678976767n;
+// console.log(BigInt);
+
+//2.non-primitive
+
+//object
+
+// let Student ={
+//     name:"milan",
+//     roll:65
+// }
+// console.log(Student)
+//array
+
+// let a=[1,2,3,4,5,6,7,8,]
+
+// console.log(a);
+
+// function greet(){
+//     console.log("this is function");
+// }
+// greet();
+
+// function showValues(value){
+//     console.log("this is value:",value);
+//     if(value == 20){
+//         console.log("number");
+//     }
+// }
+// showValues();
+// showValues(20);
+// showValues([1,2,3,4,5]);
+
+// function addition(v1,v2){
+// console.log("addition is:",v1+v2)
+// console.log("substraction is :",v1-v2)
+// console.log("multiplication is:",v1*v2)
+// console.log("division is:",v1/v2)
+// }
+// addition(20,20);
+
+// const add=(num1=5, num2=5)=>{
+//     return num1+num2
+//     console.log()
+
+// }
+// console.log("this is arrow fun.:",add())
+
+// const add=(v1=5,v2=5)=>{
+//     return v1+v2
+//     console.log()
+// }
+// console.log("add is ",add())
+
+// const sub=(v1=5,v2=5)=>{
+//     return v1-v2
+//     console.log()
+// }
+// console.log("sub is ",sub())
+
+// const mul=(v1=5,v2=5)=>{
+//     return v1*v2
+//     console.log()
+// }
+// console.log("mul is ",mul())
+
+// const div=(v1=5,v2=5)=>{
+//     return v1/v2
+//     console.log()
+// }
+// console.log("div is ",div())
+
+// let value =10;
+// console.log("default value",value);
+
+// value +=5
+// console.log(value)
+
+// value -=5
+// console.log(value)
+
+// value *=5
+// console.log(value)
+
+// value /=5
+// console.log(value)
+
+// let age=20;
+// let hasId=true
+// let name="milan";
+
+// let obj={
+//     name:'abc',
+//     rollno:20
+// }
+
+// console.log("logical AND (&&)",age > 18 && hasId);
+// console.log("logical OR (||)",age < 18 || hasId);
+// console.log("logical NOT (!)", ! hasId);
+
+// console.log(typeof age)
+// console.log(typeof hasId)
+// console.log(typeof name)
+// console.log(typeof obj)
+
+let value = 10;
+let num = "10";
+
+console.log("value ==  num:",value == num);
+console.log("value === num", value === num);
+console.log("value != num", value != num);
+
+
---
+++ variable.js
@@ -0,0 +1,12 @@
+// var a=10;
+// console.log(a);
+// let a=10;
+
+// if(true){
+//  let a = 20;
+
+// }
+// console.log(a);
+
+const pi=3.14;
+console.log(pi);
