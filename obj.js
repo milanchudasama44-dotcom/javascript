f3f023a3e0691e8a0652eab87c9887e6c888e9c2
unknown <you@example.com>
2026-01-29 13:43:31 +0530

    [CM][object sourse]

--- obj.js
+++ obj.js
@@ -33,3 +33,76 @@ console.log(person1?.address?.city)
 console.log(Object.keys(person1))
 console.log(Object.values(person1))
 console.log(Object.entries(person1))
+
+//
+
+const user ={
+    name: "user",
+    role: "admin",
+    designation: "developer",
+    age:24,
+    address:{
+        city: "junagadh",
+        xipcode: '362001',
+        street:"123 main st."
+    }
+}
+// const {name, age, role}=user
+// console.log(name,age,role)
+ 
+// const {name : profilename,age: profileage}=user
+// console.log(profilename,profileage)
+
+const {name, age, role,state = "gujarat"}=user
+console.log(name,age,role,state)
+
+const array = [1,2,3]
+const moreelements = [...array,4,5,6]
+console.log(moreelements)
+
+
+const originalarray = [1,2,3]
+const copiedarray = [...originalarray]
+console.log(copiedarray)
+
+
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+const mergedarray = [...arr1,...arr2]
+console.log(mergedarray)
+
+//spread opt. in obj
+
+const user1 = {
+    name: "user1",
+    city: "junagadh"
+}
+console.log(user1)
+
+const newobj = {
+    ...user1,
+    role:"react developer",
+
+}
+console.log(newobj)
+
+//copy obj
+const user2 = {
+    name: "user2",
+    city: "junagadh"
+}
+const user3 = {...user2}
+console.log(user3)
+
+//merge obj
+const obj1 = {
+    name: "name",
+    age: 20
+}
+const obj2 ={
+    city: "junagadh",
+    state: "gujarat",
+    age: 30
+}
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
---
+++ task.js
@@ -0,0 +1,19 @@
+let student = {
+    name:"  first name  ",
+    email:"user@gmail.com",
+    course:"javascript,react,node",
+    marks: [45,78,60,90,33],
+    hobbies: ["music","cricket","reading"],
+    address: ["india",["gujarat","ahmedabad"]]
+
+};
+
+string operation
+remove extra spaces from name and convert it tc
+convert email to lowecase
+check whether course includes "react"
+find the index of "node" in coursereplace "javascript" with "js"
+convert course string into an array 
+display the length of student name
+array operations
+add "swimming" to hobbiesremove the last hobby
